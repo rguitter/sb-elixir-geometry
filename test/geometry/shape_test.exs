f5cd8b01ac31d21fdfe351892170a1fc44972350
Richard Guitter <richard.guitter.ext@proximus.com>
2017-12-16 12:47:38 +0100

    Clean the code

--- test/geometry/shape_test.exs
+++ test/geometry/shape_test.exs
@@ -4,36 +4,37 @@ defmodule Geometry.ShapeTest do
     #doctest Geometry.Shape
     #import Geometry.Shape
     require Geometry.Shape
+    alias Geometry.Shape, as: Shape
 
     test "greets the world" do
       #assert hello() == :world
     end
   
     test "not a shape area" do
-      assert Geometry.Shape.area(nil) == 0
+      assert Shape.area(nil) == 0
     end
 
     test "circle area" do
-      assert Geometry.Shape.area(%Geometry.Shape{kind: :circle, dimensions: 0}) == 0
+      assert Shape.area(%Shape{kind: :circle, dimensions: 0}) == 0
     end
 
     test "rectanble area" do
-      assert Geometry.Shape.area(%Geometry.Shape{kind: :rectangle, dimensions: %{length: 1, width: 2}}) == 2
+      assert Shape.area(%Shape{kind: :rectangle, dimensions: %{length: 1, width: 2}}) == 2
     end
 
     test "total area" do
-      assert Geometry.Shape.total_area([
-            %Geometry.Shape{kind: :circle, dimensions: 0},
-            %Geometry.Shape{kind: :rectangle, dimensions: %{length: 1, width: 2}}
+      assert Shape.total_area([
+            %Shape{kind: :circle, dimensions: 0},
+            %Shape{kind: :rectangle, dimensions: %{length: 1, width: 2}}
             ]) == 2
     end
 
     test "not a list area" do
-      assert Geometry.Shape.total_area(nil) == 0
+      assert Shape.total_area(nil) == 0
     end
   
     test "empty list area" do
-      assert Geometry.Shape.total_area([]) == 0
+      assert Shape.total_area([]) == 0
     end
     
 end
